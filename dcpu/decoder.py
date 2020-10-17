b5d24d1f629669de50987a22d855b712b5ae36fa
Vladislav Shvets <v.shvets@ivideon.com>
2020-10-17 12:43:00 +0300

    implemented most bool/math opcodes and jumps

--- dcpu/decoder.py
+++ dcpu/decoder.py
@@ -1,7 +1,7 @@
 import argparse
 from enum import Enum
 
-from constants import BIN2OPCODE, BIN2SPECTIAL
+from constants import BIN2OPCODE, BIN2SPECTIAL, BIN2REGISTERS
 
 
 class InstructionType(Enum):
@@ -19,8 +19,8 @@ class InstructionType(Enum):
         else:
             return InstructionType.SPECIAL
 
-class Decoder:
 
+class Decoder:
     def describe_instruction(self, code):
         instruction_type = InstructionType.determine(code)
 
@@ -101,20 +101,46 @@ class Decoder:
                 code = f.read(2)
 
     @staticmethod
-    def print_instruction(cmd, code, nw_a, nw_b, op_a, op_b, pc):
-        ops = ''
-        ops += '0x{:02x} '.format(op_b) if op_b else ''
-        ops += '0x{:02x}'.format(op_a) if op_a else ''
+    def print_instruction(instruction, pc):
 
-        next_words = ''
-        next_words += '0x{:04x} '.format(nw_b) if nw_b else ''
-        next_words += '0x{:04x}'.format(nw_a) if nw_a else ''
-        next_words = '[{}]'.format(next_words) if next_words else ''
+        ops = ''
+        for operator, is_a in [
+            (instruction.B, False),
+            (instruction.A, True),
+        ]:
+            if operator is None:
+                continue
+
+            op = operator.op
+            nw = operator.nw
+
+            if 0x00 <= op <= 0x07:
+                value = BIN2REGISTERS[op]
+            elif 0x08 <= op <= 0x0f:
+                value = f'[{BIN2REGISTERS[op-0x08]}]'
+            elif 0x10 <= op <= 0x17:
+                value = f'[{BIN2REGISTERS[op-0x10]} + 0x{nw:04x}]'
+            elif op == 0x18:
+                value = 'POP' if is_a else 'PUSH'
+            elif op == 0x19:
+                value = 'PEEK'
+            elif op == 0x1a:
+                value = f'PEEK 0x{nw:04x}'
+            elif 0x1b <= op <= 0x1d:
+                value = BIN2REGISTERS[op]
+            elif op == 0x1e:
+                value = f'[0x{nw:04x}]'
+            elif op == 0x1f:
+                value = f'0x{nw:04x}'
+            else:
+                raise Exception
+
+            ops += value + ' '
 
         print(
             '0x{:04x}'.format(pc),
-            '0x{:04x} 0x{:016b}'.format(code, code),
-            cmd, ops, next_words,
+            '0x{:04x} 0x{:016b}'.format(instruction.code, instruction.code),
+            instruction.cmd, ops
         )
 
     @staticmethod
--- dcpu/emulator.py
+++ dcpu/emulator.py
@@ -1,14 +1,53 @@
 import argparse
+from collections import defaultdict
 
 from constants import BIN2REGISTERS
 from decoder import Decoder
 
 
-class Registers:
-    def __init__(self):
-        pass
+class Operator:
+    def __init__(self, op, nw):
+        self.op = op
+        self.nw = nw
 
-    def _g_temh
+
+class Instruction:
+    def __init__(self, code, cmd, op_b, nw_b, op_a, nw_a):
+        self.code = code
+        self.cmd = cmd
+        self.A = Operator(op_a, nw_a)
+        self.B = Operator(op_b, nw_b) if op_b else None
+
+
+class Registers:
+    REGS = ['A', 'B', 'C', 'X', 'Y', 'Z', 'I', 'J', 'SP', 'PC', 'EX', 'IA']
+
+    def __init__(self):
+        self._regs = defaultdict(lambda: 0)
+
+    def __getitem__(self, item):
+        return self.__getattr__(item)
+
+    def __setitem__(self, key, value):
+        return self.__setattr__(key, value)
+
+    def __getattr__(self, item):
+        if item not in self.REGS:
+            raise Exception
+        return self._regs[item]
+
+    def __setattr__(self, key, value):
+        if key == '_regs':
+            super.__setattr__(self, key, value)
+            return
+
+        if key not in self.REGS:
+            raise Exception
+        self._regs[key] = value
+
+    def __repr__(self):
+        regs_data = ', '.join([f'{reg}=0x{self.__getattr__(reg):04x}' for reg in self.REGS])
+        return f'<Registers: {regs_data}>'
 
 
 class RAM:
@@ -37,111 +76,256 @@ class Emulator:
         self._debug = debug
 
         self.decoder = Decoder()
-        self.ram = RAM(debug)
-        self.regs = {reg: 0x0 for reg in ['A', 'B', 'C', 'X', 'Y', 'Z', 'I', 'J', 'SP', 'PC', 'EX']}
-        self.regs['SP'] = 0xffff
+        self.ram = RAM(False)
+        self.regs = Registers()
+        self.regs.SP = 0xffff+1
 
-    def run(self, filename):
-        if self._debug:
-            print('Loading program to RAM...')
-
-        for pc, code in self.decoder.gen_loader(filename):
-            self.ram[pc] = code
-
-        if self._debug:
-            print('Loading done.')
-
+    def gen_instrucions(self):
         while True:
-            code = self.ram[self.regs['PC']]
+            code = self.ram[self.regs.PC]
             cmd, op_b, op_a, nw_b, nw_a = self.decoder.describe_instruction(code)
 
             if nw_a is True:
-                self.regs['PC'] += 1
-                nw_a = self.ram[self.regs['PC']]
+                self.regs.PC += 1
+                nw_a = self.ram[self.regs.PC]
 
             if nw_b is True:
-                self.regs['PC'] += 1
-                nw_b = self.ram[self.regs['PC']]
+                self.regs.PC += 1
+                nw_b = self.ram[self.regs.PC]
+
+            yield Instruction(code, cmd, op_b, nw_b, op_a, nw_a)
+
+    def run(self, filename):
+        print('Loading program to RAM...')
+        for pc, code in self.decoder.gen_loader(filename):
+            self.ram[pc] = code
+        print('Loading done.')
+
+        for instruction in self.gen_instrucions():
+            if self._debug:
+                self.decoder.print_instruction(instruction, self.regs.PC)
+
+            value_b = self.get_value_from_op(instruction.B, is_a=False)
+            value_a = self.get_value_from_op(instruction.A, is_a=True)
+
+            do_not_inc_pc = self.exec_instruction(instruction, value_b, value_a)
 
             if self._debug:
-                print('Executing 0x{:04x} "{} {} {} {} {}"'.format(self.regs['PC'], cmd, op_b, op_a, nw_b, nw_a))
+                print(self.regs)
 
-                if cmd == 'SET':
-                    self.set(op_b, op_a, nw_b, nw_a)
-                elif cmd == 'JSR':
-                    self.push(self.regs['PC'] + 1)
-                    self.regs['PC'] = nw_a
-                    continue
-                elif cmd == 'ADD':
-                    value_b = self.get_value_from_op(nw_b, op_b)
-                    value_a = self.get_value_from_op(nw_a, op_a)
-                    self.set(op_b, nw_b, exact_value=value_a + value_b)
-                else:
-                    raise Exception
+            if do_not_inc_pc is False:
+                self.regs.PC += 1
 
-            print(self.regs)
-            self.regs['PC'] += 1
+    def exec_instruction(self, instruction, value_b, value_a):
+        cmd = instruction.cmd
 
-    def set(self, op_b, op_a=None, nw_b=None, nw_a=None, exact_value=None):
-        value = self.get_value_from_op(nw_a, op_a)
-        if exact_value:
-            value = exact_value
+        do_not_inc_pc = False
+        if cmd == 'SET':
+            self.set(instruction.B, value_a)
+            if instruction.B.op == 0x1c:  # PC
+                do_not_inc_pc = True
+        elif cmd == 'ADD':
+            self.set(instruction.B, value=value_a + value_b)
 
-        if 0x00 <= op_b <= 0x07:
-            self.regs[BIN2REGISTERS[op_b]] = value
-        elif 0x08 <= op_b <= 0x0f:
-            self.ram[self.regs[BIN2REGISTERS[op_b]]] = value
-        elif 0x10 <= op_b <= 0x17:
-            self.ram[self.regs[BIN2REGISTERS[op_b] + nw_b]] = value
-        elif op_b == 0x18:
+            if value_a + value_b > 0xffff:
+                self.regs.EX = 0x0001
+            else:
+                self.regs.EX = 0x0000
+
+        elif cmd == 'SUB':
+            self.set(instruction.B, value=value_b - value_a)
+
+            if value_b - value_a < 0x0000:
+                self.regs.EX = 0xffff
+            else:
+                self.regs.EX = 0x0000
+        elif cmd == 'MUL' or cmd == 'MLI':  # ?
+            self.set(instruction.B, value=value_b * value_a)
+            self.regs.EX = ((value_b * value_a) >> 16) & 0xffff
+        elif cmd == 'DIV' or cmd == 'DVI':
+            if value_a == 0x0:
+                self.set(instruction.B, value=0)
+            else:
+                self.set(instruction.B, value=int(value_b / value_a))
+        elif cmd == 'MOD' or cmd == 'MDI':
+            if value_a == 0x0:
+                self.set(instruction.B, value=0)
+            else:
+                self.set(instruction.B, value=value_b % value_a)
+        elif cmd == 'AND':
+            self.set(instruction.B, value=value_b & value_a)
+        elif cmd == 'BOR':
+            self.set(instruction.B, value=value_b | value_a)
+        elif cmd == 'XOR':
+            self.set(instruction.B, value=value_b ^ value_a)
+        elif cmd == 'SHR':
+            self.set(instruction.B, value=value_b >> value_a)
+        elif cmd == 'SHL':
+            self.set(instruction.B, value=value_b << value_a)
+
+        elif cmd == 'IFB':
+            if value_a & value_b == 0:
+                self.skip_next_instruction()
+        elif cmd == 'IFB':
+            if value_a & value_b != 0:
+                self.skip_next_instruction()
+        elif cmd == 'IFE':
+            if value_a != value_b:
+                self.skip_next_instruction()
+        elif cmd == 'IFN':
+            if value_a == value_b:
+                self.skip_next_instruction()
+        elif cmd == 'IFG' or cmd == 'IFA':
+            if value_a <= value_b:
+                self.skip_next_instruction()
+        elif cmd == 'IFL' or cmd == 'IFU':
+            if value_a >= value_b:
+                self.skip_next_instruction()
+
+        elif cmd == 'ADX':
+            tmp = value_b + value_a + self.regs.EX
+            self.set(instruction.B, value=tmp)
+            if tmp > 0xffff:
+                self.regs.EX = 0x0001
+            else:
+                self.regs.EX = 0x0000
+        elif cmd == 'SBX':
+            tmp = value_b - value_a + self.regs.EX
+            self.set(instruction.B, value=tmp)
+            if tmp < 0x0000:
+                self.regs.EX = 0xffff
+            else:
+                self.regs.EX = 0x0000
+        elif cmd == 'STI':
+            self.set(instruction.B, value=value_a)
+            self.regs.I += 1
+            self.regs.J += 1
+        elif cmd == 'SDI':
+            self.set(instruction.B, value=value_a)
+            self.regs.I -= 1
+            self.regs.J -= 1
+
+        # special
+        elif cmd == 'JSR':
+            self.push(self.regs.PC + 1)
+            self.regs.PC = instruction.A.nw
+            do_not_inc_pc = True
+
+        elif cmd == 'INT':
+            print(' ** todo **')
+            pass
+        elif cmd == 'IAG':
+            self.set(instruction.A, value=self.regs.IA)
+        elif cmd == 'IAS':
+            self.regs.IA = value_a
+        elif cmd == 'RFI':
+            print(' ** todo **')
+            self.regs.A = self.pop()
+            self.regs.PC = self.pop()
+            do_not_inc_pc = True
+        elif cmd == 'IAQ':
+            print(' ** todo **')
+            pass
+        elif cmd == 'HWN':
+            print(' ** todo **')
+            pass
+        elif cmd == 'HWQ':
+            print(' ** todo **')
+            pass
+        elif cmd == 'HWI':
+            print(' ** todo **')
+            pass
+        else:
+            raise Exception
+        return do_not_inc_pc
+
+    def skip_next_instruction(self):
+        code = self.ram[self.regs.PC + 1]
+        cmd, op_b, op_a, nw_b, nw_a = self.decoder.describe_instruction(code)
+        skip = 1
+        if nw_a:
+            skip += 1
+        if nw_b:
+            skip += 1
+
+        self.regs.PC += skip
+
+    def set(self, operator: Operator, value=None):
+        op = operator.op
+        nw = operator.nw
+        if 0x00 <= op <= 0x07:
+            self.regs[BIN2REGISTERS[op]] = value
+        elif 0x08 <= op <= 0x0f:
+            self.ram[self.regs[BIN2REGISTERS[op - 0x08]]] = value
+        elif 0x10 <= op <= 0x17:
+            self.ram[self.regs[BIN2REGISTERS[op - 0x10] + nw]] = value
+        elif op == 0x18:
             self.push(value)
-        elif op_b == 0x19:
-            self.ram[self.regs['SP']] = value
-        elif op_b == 0x1a:
-            self.ram[self.regs['SP'] + nw_b] = value
-        elif 0x1b <= op_b <= 0x1d:
-            self.regs[BIN2REGISTERS[op_b]] = value
-        elif op_b == 0x1e:
-            self.ram[nw_b] = value
-        elif op_b == 0x1f:
+        elif op == 0x19:
+            self.ram[self.regs.SP] = value
+        elif op == 0x1a:
+            self.ram[self.regs.SP + nw] = value
+        elif 0x1b <= op <= 0x1d:
+            self.regs[BIN2REGISTERS[op]] = value
+        elif op == 0x1e:
+            self.ram[nw] = value
+        elif op == 0x1f:
             raise Exception
         else:
             raise Exception
 
-    def get_value_from_op(self, nw_a, op_a):
-        if 0x00 <= op_a <= 0x07:
-            value = self.regs[BIN2REGISTERS[op_a]]
-        elif 0x08 <= op_a <= 0x0f:
-            value = self.ram[self.regs[BIN2REGISTERS[op_a]]]
-        elif 0x10 <= op_a <= 0x17:
-            value = self.ram[self.regs[BIN2REGISTERS[op_a] + nw_a]]
-        elif op_a == 0x18:
-            value = self.pop()
-        elif op_a == 0x19:
-            value = self.peek()
-        elif op_a == 0x1a:
-            value = self.peek(nw_a)
-        elif 0x1b <= op_a <= 0x1d:
-            value = self.regs[BIN2REGISTERS[op_a]]
-        elif op_a == 0x1e:
-            value = self.ram[nw_a]
-        elif op_a == 0x1f:
-            value = nw_a
+    def get_value_from_op(self, operator, is_a=False):
+        if operator is None:
+            return None
+
+        op = operator.op
+        nw = operator.nw
+
+        if 0x00 <= op <= 0x07:
+            return self.regs[BIN2REGISTERS[op]]
+        elif 0x08 <= op <= 0x0f:
+            return self.ram[self.regs[BIN2REGISTERS[op - 0x08]]]
+        elif 0x10 <= op <= 0x17:
+            return self.ram[self.regs[BIN2REGISTERS[op - 0x10] + nw]]
+        elif op == 0x18:
+            return self.pop() if is_a else None
+        elif op == 0x19:
+            return self.peek()
+        elif op == 0x1a:
+            return self.peek(nw)
+        elif 0x1b <= op <= 0x1d:
+            return self.regs[BIN2REGISTERS[op]]
+        elif op == 0x1e:
+            return self.ram[nw]
+        elif op == 0x1f:
+            return nw
         else:
             raise Exception
-        return value
 
     def push(self, value):
-        self.regs['SP'] -= 1
-        self.ram[self.regs['SP']] = value
+        self.regs.SP -= 1
+        self.ram[self.regs.SP] = value
+        self._dump_stack()
 
     def pop(self):
-        value = self.ram[self.regs['SP']]
-        self.regs['SP'] += 1
+        self._dump_stack()
+        value = self.ram[self.regs.SP]
+        self.regs.SP += 1
         return value
 
     def peek(self, n=0):
-        return self.ram[self.regs['SP'] + n]
+        return self.ram[self.regs.SP + n]
+
+    def _dump_stack(self):
+        i = 0xffff
+        print('Stack: ')
+
+        while i >= self.regs.SP:
+            pointer = '<--------------' if i == self.regs.SP else ''
+
+            print(f'[0x{i:04x}] 0x{self.ram[i]:04x} {pointer}')
+            i -= 1
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
