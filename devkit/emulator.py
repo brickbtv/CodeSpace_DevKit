503215a5d70f7dcfb1699083a32c5f60a9d9150f
Vladislav Shvets <v.shvets@ivideon.com>
2020-10-20 18:26:57 +0300

    moved instructions handling to functions

--- devkit/emulator.py
+++ devkit/emulator.py
@@ -1,9 +1,33 @@
 import argparse
+from functools import wraps
 
 from constants import BIN2REGISTERS
 from decoder import load_bin_file, to_human_readable, to_human_readable_dat, describe_instruction
 from hardware import Display, Keyboard, RAM, Registers, Sensor, Thruster
 from instuction import Operator, Instruction
+
+
+HANDLERS = {}
+
+
+def instruction(func):
+    """ Decorator for instructions.
+
+        Wrapped function should return bool value, which means to
+        increase PC counter or not after performing the instruction.
+
+        Defaults to False.
+    """
+    @wraps(func)
+    def wrapper(self, instruction: Instruction, value_b: int, value_a: int):
+        do_increase_pc = func(self, instruction, value_b, value_a)
+        if do_increase_pc is None:
+            return False
+
+        return do_increase_pc
+
+    HANDLERS[func.__name__.replace('_', '').upper()] = wrapper
+    return wrapper
 
 
 class Emulator:
@@ -13,22 +37,23 @@ class Emulator:
         self.ram = RAM()
         self.regs = Registers()
         self.regs.SP = 0xffff + 1
+
         self.hardware = []
         self.hardware.extend([Thruster(self.regs, self.ram) for _ in range(8)])
         self.hardware.extend([Sensor(self.regs, self.ram)])
         self.hardware.extend([Keyboard(self.regs, self.ram), Display(self.regs, self.ram)])
-        self.on_interruption = False
+        self.on_interruption_now = False
 
     def gen_instructions(self):
         while True:
             keyboard = self.hardware[-2]
-            if keyboard.interruptions and self.on_interruption is False:
+            if keyboard.interruptions and self.on_interruption_now is False:
                 self.push(self.regs.PC)
                 self.push(self.regs.A)
 
                 self.regs.A = keyboard.interruptions.pop()
                 self.regs.PC = self.regs.IA
-                self.on_interruption = True
+                self.on_interruption_now = True
 
             origin_pc = self.regs.PC
             code = self.ram[self.regs.PC]
@@ -51,8 +76,6 @@ class Emulator:
         for step in self.step_run(filename):
             if self._debug:
                 print(self.regs)
-
-            self.hardware[-1].dump()
 
     def step_run(self, filename):
         print('Loading program to RAM...')
@@ -77,136 +100,213 @@ class Emulator:
 
             yield pc
 
-    def exec_instruction(self, instruction, value_b, value_a):
-        cmd = instruction.cmd
+    @instruction
+    def add(self, inst, b, a):
+        self.set(inst.B, value=a + b)
 
-        do_not_inc_pc = False
-        if cmd == 'SET':
-            self.set(instruction.B, value_a)
-            if instruction.B.op == 0x1c:  # PC
-                do_not_inc_pc = True
-        elif cmd == 'ADD':
-            self.set(instruction.B, value=value_a + value_b)
+        if b + a > 0xffff:
+            self.regs.EX = 0x0001
+        else:
+            self.regs.EX = 0x0000
 
-            if value_a + value_b > 0xffff:
-                self.regs.EX = 0x0001
-            else:
-                self.regs.EX = 0x0000
+    @instruction
+    def sub(self, inst, b, a):
+        self.set(inst.B, value=b - a)
 
-        elif cmd == 'SUB':
-            self.set(instruction.B, value=value_b - value_a)
+        if b - a < 0x0000:
+            self.regs.EX = 0xffff
+        else:
+            self.regs.EX = 0x0000
 
-            if value_b - value_a < 0x0000:
-                self.regs.EX = 0xffff
-            else:
-                self.regs.EX = 0x0000
-        elif cmd == 'MUL' or cmd == 'MLI':  # ?
-            self.set(instruction.B, value=value_b * value_a)
-            self.regs.EX = ((value_b * value_a) >> 16) & 0xffff
-        elif cmd == 'DIV' or cmd == 'DVI':
-            if value_a == 0x0:
-                self.set(instruction.B, value=0)
-            else:
-                self.set(instruction.B, value=int(value_b / value_a))
-        elif cmd == 'MOD' or cmd == 'MDI':
-            if value_a == 0x0:
-                self.set(instruction.B, value=0)
-            else:
-                self.set(instruction.B, value=value_b % value_a)
-        elif cmd == 'AND':
-            self.set(instruction.B, value=value_b & value_a)
-        elif cmd == 'BOR':
-            self.set(instruction.B, value=value_b | value_a)
-        elif cmd == 'XOR':
-            self.set(instruction.B, value=value_b ^ value_a)
-        elif cmd == 'SHR':
-            self.set(instruction.B, value=value_b >> value_a)
-        elif cmd == 'SHL':
-            self.set(instruction.B, value=value_b << value_a)
+    @instruction
+    def set(self, inst, _, a):
+        self.set(inst.B, a)
+        return inst.B.op == 0x1c  # SET PC, x must not increase PC
 
-        elif cmd == 'IFB':
-            if value_b & value_a == 0:
-                self.skip_next_instruction()
-        elif cmd == 'IFC':
-            if value_b & value_a != 0:
-                self.skip_next_instruction()
-        elif cmd == 'IFE':
-            if value_b != value_a:
-                self.skip_next_instruction()
-        elif cmd == 'IFN':
-            if value_b == value_a:
-                self.skip_next_instruction()
-        elif cmd == 'IFG' or cmd == 'IFA':
-            if value_b <= value_a:
-                self.skip_next_instruction()
-        elif cmd == 'IFL' or cmd == 'IFU':
-            if value_b >= value_a:
-                self.skip_next_instruction()
+    @instruction
+    def mul(self, inst, b, a):
+        self.set(inst.B, value=b * a)
+        self.regs.EX = ((b * a) >> 16) & 0xffff
 
-        elif cmd == 'ADX':
-            tmp = value_b + value_a + self.regs.EX
-            self.set(instruction.B, value=tmp)
-            if tmp > 0xffff:
-                self.regs.EX = 0x0001
-            else:
-                self.regs.EX = 0x0000
-        elif cmd == 'SBX':
-            tmp = value_b - value_a + self.regs.EX
-            self.set(instruction.B, value=tmp)
-            if tmp < 0x0000:
-                self.regs.EX = 0xffff
-            else:
-                self.regs.EX = 0x0000
-        elif cmd == 'STI':
-            self.set(instruction.B, value=value_a)
-            self.regs.I += 1
-            self.regs.J += 1
-        elif cmd == 'SDI':
-            self.set(instruction.B, value=value_a)
-            self.regs.I -= 1
-            self.regs.J -= 1
+    @instruction
+    def mli(self, inst, b, a):
+        print('**TODO**')
+        return self.mul(inst, b, a)
 
-        # special
-        elif cmd == 'JSR':
-            self.push(self.regs.PC + 1)
-            self.regs.PC = instruction.A.nw
-            do_not_inc_pc = True
+    @instruction
+    def div(self, inst, b, a):
+        if a == 0x0:
+            self.set(inst.B, value=0)
+        else:
+            self.set(inst.B, value=int(b / a))
 
-        elif cmd == 'INT':
-            print(' ** todo **')
-            pass
-        elif cmd == 'IAG':
-            self.set(instruction.A, value=self.regs.IA)
-        elif cmd == 'IAS':
-            self.regs.IA = value_a
-        elif cmd == 'RFI':
-            self.regs.A = self.pop()
-            self.regs.PC = self.pop()
-            do_not_inc_pc = True
-            self.on_interruption = False
-        elif cmd == 'IAQ':
-            print(' ** todo **')
-            pass
-        elif cmd == 'HWN':
-            self.set(instruction.A, value=len(self.hardware))
-        elif cmd == 'HWQ':
-            hwnum = value_a
-            hwid = self.hardware[hwnum].ID
-            vendor = self.hardware[hwnum].VENDOR
+    @instruction
+    def dvi(self, inst, b, a):
+        print('**TODO**')
+        return self.div(inst, b, a)
 
-            self.regs.A = hwid & 0xffff
-            self.regs.B = hwid >> 16
+    @instruction
+    def mod(self, inst, b, a):
+        if a == 0x0:
+            self.set(inst.B, value=0)
+        else:
+            self.set(inst.B, value=b % a)
 
-            self.regs.C = self.hardware[hwnum].VERSION
-            self.regs.X = vendor & 0xffff
-            self.regs.Y = vendor >> 16
-        elif cmd == 'HWI':
-            hwnum = value_a
-            device = self.hardware[hwnum]
-            device.handle_interruption()
+    @instruction
+    def mdi(self, inst, b, a):
+        return self.mod(inst, b, a)
+
+    @instruction
+    def and_(self, inst, b, a):
+        print('**TODO**')
+        self.set(inst.B, value=b & a)
+
+    @instruction
+    def bor(self, inst, b, a):
+        self.set(inst.B, value=b | a)
+
+    @instruction
+    def xor(self, inst, b, a):
+        self.set(inst.B, value=b ^ a)
+
+    @instruction
+    def shr(self, inst, b, a):
+        self.set(inst.B, value=b >> a)
+
+    @instruction
+    def shl(self, inst, b, a):
+        self.set(inst.B, value=b << a)
+
+    @instruction
+    def ifb(self, _, b, a):
+        if b & a == 0:
+            self.skip_next_instruction()
+
+    @instruction
+    def ifc(self, _, b, a):
+        if b & a != 0:
+            self.skip_next_instruction()
+
+    @instruction
+    def ife(self, _, b, a):
+        if b != a:
+            self.skip_next_instruction()
+
+    @instruction
+    def ifn(self, _, b, a):
+        if b == a:
+            self.skip_next_instruction()
+
+    @instruction
+    def ifg(self, _, b, a):
+        if b <= a:
+            self.skip_next_instruction()
+
+    @instruction
+    def ifa(self, _, b, a):
+        print('**TODO**')
+        return self.ifg(_, b, a)
+
+    @instruction
+    def ifl(self, _, b, a):
+        if b >= a:
+            self.skip_next_instruction()
+
+    @instruction
+    def ifu(self, _, b, a):
+        print('**TODO**')
+        return self.ifl(_, b, a)
+
+    @instruction
+    def adx(self, inst, b, a):
+        tmp = b + a + self.regs.EX
+        self.set(inst.B, value=tmp)
+        if tmp > 0xffff:
+            self.regs.EX = 0x0001
+        else:
+            self.regs.EX = 0x0000
+
+    @instruction
+    def sbx(self, inst, b, a):
+        tmp = b - a + self.regs.EX
+        self.set(inst.B, value=tmp)
+        if tmp < 0x0000:
+            self.regs.EX = 0xffff
+        else:
+            self.regs.EX = 0x0000
+
+    @instruction
+    def sti(self, inst, _, a):
+        self.set(inst.B, value=a)
+        self.regs.I += 1
+        self.regs.J += 1
+
+    @instruction
+    def sdi(self, inst, _, a):
+        self.set(inst.B, value=a)
+        self.regs.I -= 1
+        self.regs.J -= 1
+
+    @instruction
+    def jsr(self, _, __, a):
+        self.push(self.regs.PC + 1)
+        self.regs.PC = a
+        return True
+
+    @instruction
+    def int_(self, inst, b, a):
+        print('**TODO**')
+
+    @instruction
+    def iag(self, inst, _, __):
+        self.set(inst.A, value=self.regs.IA)
+
+    @instruction
+    def ias(self, _, __, a):
+        self.regs.IA = a
+
+    @instruction
+    def rfi(self, _, __, ___):
+        self.regs.A = self.pop()
+        self.regs.PC = self.pop()
+        self.on_interruption_now = False
+
+        return True
+
+    @instruction
+    def iaq(self, inst, b, a):
+        print('**TODO**')
+
+    @instruction
+    def hwn(self, inst, _, __):
+        self.set(inst.A, value=len(self.hardware))
+
+    @instruction
+    def hwq(self, _, __, a):
+        hwnum = a
+        hwid = self.hardware[hwnum].ID
+        vendor = self.hardware[hwnum].VENDOR
+
+        self.regs.A = hwid & 0xffff
+        self.regs.B = hwid >> 16
+
+        self.regs.C = self.hardware[hwnum].VERSION
+        self.regs.X = vendor & 0xffff
+        self.regs.Y = vendor >> 16
+
+    @instruction
+    def hwi(self, _, __, a):
+        hwnum = a
+        device = self.hardware[hwnum]
+        device.handle_interruption()
+
+    def exec_instruction(self, inst, b, a):
+        cmd = inst.cmd
+
+        if cmd in HANDLERS:
+            return HANDLERS[cmd](self, inst, b, a)
         else:
             raise Exception
-        return do_not_inc_pc
 
     def skip_next_instruction(self):
         code = self.ram[self.regs.PC + 1]
@@ -283,26 +383,14 @@ class Emulator:
     def push(self, value):
         self.regs.SP -= 1
         self.ram[self.regs.SP] = value
-        #self._dump_stack()
 
     def pop(self):
-        #self._dump_stack()
         value = self.ram[self.regs.SP]
         self.regs.SP += 1
         return value
 
     def peek(self, n=0):
         return self.ram[self.regs.SP + n]
-
-    def _dump_stack(self):
-        i = 0xffff
-        print('Stack: ')
-
-        while i >= self.regs.SP:
-            pointer = '<--------------' if i == self.regs.SP else ''
-
-            print(f'[0x{i:04x}] 0x{self.ram[i]:04x} {pointer}')
-            i -= 1
 
 
 if __name__ == '__main__':
